15e4f4785e6f365f020a3464112146dd1101736d
Usuario1 <Usuario1@DESKTOP-PPN13H4>
2023-03-14 07:52:24 -0500

    Agregar archivos de proyecto.

---
+++ LibroSession/Controllers/LibroController.cs
@@ -0,0 +1,123 @@
+﻿using LibroSession.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LibroSession.Session;
+
+namespace LibroSession.Controllers
+{
+    public class LibroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(listaLibros());
+        }
+        public List<Libro> listaLibros()
+        {
+            List<Libro> list = SessionHelper.GetObjectFromJson<List<Libro>>(HttpContext.Session, "libros");
+            if (list == null)
+            {
+                list = new List<Libro>();
+            }
+            return list;
+        }
+
+        // GET: LibroController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LibroController/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Guardar(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    List<Libro> list = SessionHelper.GetObjectFromJson<List<Libro>>(HttpContext.Session, "libros");
+                    if (list == null)
+                        list = new List<Libro>();
+                    list.Add(libro);
+                    SessionHelper.SetObjectAsJson(HttpContext.Session, "libros", list);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    TempData["errorCedula"] = ex.Message;
+                    return View("Create", libro);
+                }
+
+
+            }
+            else
+            {
+
+                return View("Create", libro);
+            }
+
+        }
+
+        // POST: LibroController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LibroController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LibroController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LibroController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LibroController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ LibroSession/Models/Libro.cs
@@ -0,0 +1,31 @@
+﻿namespace LibroSession.Models
+{
+    public class Libro
+    {
+        private String autor;
+        private String titulo;
+        private String materia;
+        private String estado;
+        private String tipo;
+        private String categoria;
+
+        public Libro() { }
+
+        public Libro(string autor, string titulo, string materia, string estado, string tipo, string categoria)
+        {
+            this.autor = autor;
+            this.titulo = titulo;
+            this.materia = materia;
+            this.estado = estado;
+            this.tipo = tipo;
+            this.categoria = categoria;
+        }
+
+        public string Autor { get => autor; set => autor = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Materia { get => materia; set => materia = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Categoria { get => categoria; set => categoria = value; }
+    }
+}
